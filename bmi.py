a69b7457c7e6e9b1e340b039c98e5018b7c854a5
Md Ansar choudhari <mdansarchoudry@gmail.com>
2025-11-11 11:09:10 +0530

    Fix argument count check for BMI calculation

--- bmi.py
+++ bmi.py
@@ -1,5 +1,5 @@
 import sys
-if len(sys.argv) == 2:
+if len(sys.argv) == 3:
     script_name = sys.argv[0]
     weight = sys.argv[1]
     height = sys.argv[2]
@@ -7,10 +7,10 @@ else:
     script_name = sys.argv[0]
     weight = 60
     height = 170
-print("Invalid input using default values")
-weight = float(weight)
-height = float(height) / 100
-bmi = weight / (height*height)
-print("Weight", weight, "kg")
-print("Height", height, "m")
-print("BMI", bmi)
+    print("Invalid input using default values")
+    weight = float(weight)
+    height = float(height) / 100
+    bmi = weight / (height*height)
+    print("Weight", weight, "kg")
+    print("Height", height, "m")
+    print("BMI", bmi)
