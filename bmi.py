c05c383c920cd45764864b07af4483548002158e
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2025-11-11 10:53:58 +0530

    save

--- bmi.py
+++ bmi.py
@@ -1,20 +1,16 @@
 import sys
-
-if len(sys.argv)==3:
-    script_name=sys.argv[0]
-    weight=sys.argv[1]
-    height=sys.argv[2]
-
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    weight = sys.argv[1]
+    height = sys.argv[2]
 else:
-    script_name=sys.argv[0]
-    weight=60
-    height=6
-
-
-
-bmi=weight/(height*height)
-
-print("Script Name : ",script_name)
-print("Weight",weight)
-print("Height",height)
-print("BMI ",bmi)
+    script_name = sys.argv[0]
+    weight = 60
+    height = 170
+print("Invalid input using default values")
+weight = float(weight)
+height = float(height) / 100
+bmi = weight / (height*height)
+print("Weight", weight, "kg")
+print("Height", height, "m")
+print("BMI", bmi)
